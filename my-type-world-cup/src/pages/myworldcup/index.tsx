bdd0f01fe214793045405c967808be8fb247e5e9
nakjun12 <wns450@gmail.com>
2023-08-07 00:58:01 +0900

    refactor : 스크롤, 삭제 모달 추가

--- my-type-world-cup/src/pages/myworldcup/index.tsx
+++ my-type-world-cup/src/pages/myworldcup/index.tsx
@@ -6,106 +6,139 @@ import SortButtons from "@/components/main/SortButtons";
 import { fetcherToken } from "@/lib/Helper";
 import { BACK_URL } from "@/lib/config";
 import { MainWorldcup, WorldcupsResponse } from "@/type/Types";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
-import { useRecoilValue, useResetRecoilState, useSetRecoilState } from "recoil";
+import {
+	useRecoilValue,
+	useResetRecoilState,
+	useSetRecoilState
+} from "recoil";
 import useSWRInfinite from "swr/infinite";
-import { accessTokenState, postWorldcup, userState } from "../../lib/atom/atom";
+import { accessTokenState, postWorldcup } from "../../lib/atom/atom";
 const PAGE_SIZE = 10;
 
 export type Value = "playCount" | "createdAt" | "commentCount";
 
 export default function Home({}: {}) {
-  const router = useRouter();
-  const accessToken = useRecoilValue(accessTokenState);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [isOpen, setIsOpen] = useState<boolean>(false); //메뉴바 관리
-  const [sort, setSort] = useState<Value>("playCount"); //정렬 관리
-  const [search, setSearch] = useState<string>(""); //검색 관리
-  const [alarm, setAlarm] = useState<boolean>(false); //알람 관리
-  const resetEditor = useResetRecoilState(postWorldcup);
-  const setEditor = useSetRecoilState(postWorldcup);
-  const memberId = useSetRecoilState(userState);
-  useEffect(() => {
-    if (!accessToken) {
-      router.push("/");
-    }
-    console.log(accessToken);
-  }, [accessToken, router]);
-  const { data, mutate, setSize } = useSWRInfinite<WorldcupsResponse>(
-    (index) =>
-      `${BACK_URL}/members/worldcups?page=${
-        index + 1
-      }&size=10&sort=${sort}${search}`,
-    (url: string) => fetcherToken(url, accessToken)
-  );
+	const router = useRouter();
+	const accessToken = useRecoilValue(accessTokenState);
+	const containerRef = useRef<HTMLDivElement>(null);
 
-  const isReachingEnd = data && data[data.length - 1]?.data.length < PAGE_SIZE;
-  useEffect(() => {
-    const handleScroll = () => {
-      const { scrollTop, scrollHeight, clientHeight } = containerRef.current!;
-      if (scrollTop + clientHeight >= scrollHeight && !isReachingEnd) {
-        setSize((el) => el + 1);
-        console.log("하이");
-      }
-    };
-    const container = containerRef.current!;
-    container.addEventListener("scroll", handleScroll);
+	const [sort, setSort] = useState<Value>("playCount"); //정렬 관리
+	const [search, setSearch] = useState<string>(""); //검색 관리
 
-    return () => {
-      return container.removeEventListener("scroll", handleScroll);
-    };
-  }, [isReachingEnd, setSize]);
+	const resetEditor = useResetRecoilState(postWorldcup);
+	const setEditor = useSetRecoilState(postWorldcup);
 
-  const handlerDelete = async (id: number) => {
-    try {
-      const res = await delete_worldcup(accessToken || "", id);
+	useEffect(() => {
+		if (!accessToken) {
+			router.push("/");
+		}
+		console.log(accessToken);
+	}, [accessToken, router]);
+	const { data, mutate, setSize } = useSWRInfinite<WorldcupsResponse>(
+		(index) =>
+			`${BACK_URL}/members/worldcups?page=${
+				index + 1
+			}&size=10&sort=${sort}${search}`,
+		(url: string) => fetcherToken(url, accessToken)
+	);
 
-      mutate();
+	const isReachingEnd =
+		data && data[data.length - 1]?.data.length < PAGE_SIZE;
+	useEffect(() => {
+		const handleScroll = () => {
+			const { scrollTop, scrollHeight, clientHeight } =
+				containerRef.current!;
+			if (
+				scrollTop + clientHeight >= scrollHeight &&
+				!isReachingEnd
+			) {
+				setSize((el) => el + 1);
+				console.log("하이");
+			}
+		};
+		const container = containerRef.current!;
+		container.addEventListener("scroll", handleScroll);
 
-      console.log(res);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  const handlerWorldCup = (id: number) => {
-    //월드컵 수정
-    resetEditor();
-    get_detail(id, accessToken || "").then((res) => {
-      console.log(res);
-      setEditor(res);
-      router.push("/editors");
-    });
-  };
-  const worldcups: MainWorldcup[] = data ? data.map((v) => v.data).flat() : [];
-  //예시는 모두다 배열임
+		return () => {
+			return container.removeEventListener("scroll", handleScroll);
+		};
+	}, [isReachingEnd, setSize]);
 
-  return (
-    <>
-      <main
-        className="flex h-screen flex-col pt-4 overflow-y-scroll relative mt-20"
-        ref={containerRef}
-      >
-        <SearchBar setSearch={setSearch} />
-        <div className="mt-8 mx-auto">
-          <SortButtons setSort={setSort} sort={sort} />
-        </div>
-        <article className="w-full h-auto ">
-          {data
-            ? worldcups.map((v) => (
-                <Card
-                  key={v.id}
-                  worldcup={v}
-                  mine={true}
-                  handlerDelete={handlerDelete}
-                  handlerWorldCup={handlerWorldCup}
-                />
-              ))
-            : new Array(10).fill(1).map((_, i) => {
-                return <CardSkeleton key={i} />;
-              })}
-        </article>
-      </main>
-    </>
-  );
+	const handlerDelete = async (id: number) => {
+		try {
+			const res = await delete_worldcup(accessToken || "", id);
+
+			mutate();
+
+			console.log(res);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	const handlerWorldCup = (id: number) => {
+		//월드컵 수정
+		resetEditor();
+		get_detail(id, accessToken || "").then((res) => {
+			console.log(res);
+			setEditor(res);
+			router.push("/editors");
+		});
+	};
+
+	const worldcups: MainWorldcup[] = data
+		? data.map((v) => v.data).flat()
+		: [];
+	//예시는 모두다 배열임
+
+	return (
+		<main
+			className='flex h-screen flex-col overflow-y-scroll relative pt-24'
+			ref={containerRef}>
+			<SearchBar setSearch={setSearch} />
+			<div className='mt-12 mx-auto'>
+				<SortButtons setSort={setSort} sort={sort} />
+			</div>
+			<article className='w-full h-auto '>
+				{data ? (
+					worldcups.length > 0 ? (
+						worldcups.map((v) => (
+							<Card
+								key={v.id}
+								worldcup={v}
+								mine={true}
+								handlerDelete={handlerDelete}
+								handlerWorldCup={handlerWorldCup}
+							/>
+						))
+					) : (
+						<div className='flex justify-center items-center flex-col mt-24 pb-[25px]'>
+							<Image
+								src='/icon/blueDolphin2.svg'
+								alt='one'
+								width={150}
+								height={250}
+							/>
+							<h3 className='text-xl mt-8 text-center leading-loose tracking-wide'>
+								월드컵이 없습니다. <br />
+								<span
+									onClick={() => router.push("/editors")}
+									className='bg-main p-2 text-white cursor-pointer rounded-lg'>
+									후보 추가
+								</span>
+								를 통해 업데이트해주세요
+							</h3>
+						</div>
+					)
+				) : (
+					new Array(10).fill(1).map((_, i) => {
+						return <CardSkeleton key={i} />;
+					})
+				)}
+			</article>
+		</main>
+	);
 }
