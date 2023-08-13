f66f8eeac1e24b331b61fa3cab62caca2c8b4586
nakjun12 <wns450@gmail.com>
2023-08-13 21:03:19 +0900

    1.0.11 버전 업데이트

---
+++ my-type-world-cup/src/lib/hooks/useWorldcups.ts
@@ -0,0 +1,49 @@
+import { fetcher, fetcherToken } from "@/lib/Helper";
+import { MainWorldcup, WorldcupsResponse } from "@/type/Types";
+import { useEffect, useRef, useState } from "react";
+import useSWRInfinite from "swr/infinite";
+import { BACK_URL } from "../config";
+
+export type SortValue = "playCount" | "createdAt" | "commentCount";
+
+export function useWorldcups(url: string, token?: string | null) {
+	const containerRef = useRef<HTMLDivElement>(null);
+	const [sort, setSort] = useState<SortValue>("playCount");
+	const [search, setSearch] = useState<string>("");
+
+	const { data, setSize, isValidating } = useSWRInfinite<WorldcupsResponse>(
+		(index) =>
+			`${BACK_URL}${url}?page=${index + 1}&size=10&sort=${sort}${search}`,
+		token ? (url: string) => fetcherToken(url, token) : fetcher
+	);
+
+	const isReachingEnd = data && data[data.length - 1]?.data.length < 10;
+	const isLoading = !data && isValidating; // 로딩 상태를 나타내는 변수
+	const worldcups: MainWorldcup[] = data ? data.map((v) => v.data).flat() : [];
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const { scrollTop, scrollHeight, clientHeight } = containerRef.current!;
+
+			if (scrollTop + clientHeight >= scrollHeight && !isReachingEnd) {
+				setSize((el) => el + 1);
+			}
+		};
+		const container = containerRef.current!;
+		container.addEventListener("scroll", handleScroll);
+
+		return () => {
+			return container.removeEventListener("scroll", handleScroll);
+		};
+	}, [isReachingEnd, setSize]);
+
+	return {
+		containerRef,
+		sort,
+		setSort,
+		search,
+		setSearch,
+		worldcups,
+		isLoading // 로딩 상태를 반환 객체에 포함
+	};
+}
--- my-type-world-cup/src/pages/index.tsx
+++ my-type-world-cup/src/pages/index.tsx
@@ -1,73 +1,18 @@
-import Card from "@/components/main/Card";
-import CardSkeleton from "@/components/main/CardSkeleton";
 import SearchBar from "@/components/main/SearchBar";
 import SortButtons from "@/components/main/SortButtons";
-import { fetcher } from "@/lib/Helper";
-import { BACK_URL } from "@/lib/config";
-import { MainWorldcup, WorldcupsResponse } from "@/type/Types";
-import { useEffect, useRef, useState } from "react";
-import useSWRInfinite from "swr/infinite";
-const PAGE_SIZE = 10;
+import WorldcupList from "@/components/main/WorldcupList";
+import { useWorldcups } from "@/lib/hooks/useWorldcups";
+export default function Home() {
+	const { containerRef, sort, setSort, setSearch, worldcups, isLoading } =
+		useWorldcups("/worldcups");
 
-export type Value = "playCount" | "createdAt" | "commentCount";
-
-export default function Home({}: {}) {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [isOpen, setIsOpen] = useState<boolean>(false); //메뉴바 관리
-  const [sort, setSort] = useState<Value>("playCount"); //정렬 관리
-  const [search, setSearch] = useState<string>(""); //검색 관리
-  console.log(sort);
-
-  const { data, mutate, size, setSize, isValidating, isLoading } =
-    useSWRInfinite<WorldcupsResponse>(
-      (index) =>
-        `${BACK_URL}/worldcups?page=${index + 1}&size=10&sort=${sort}${search}`,
-      fetcher
-    );
-
-  const isReachingEnd = data && data[data.length - 1]?.data.length < PAGE_SIZE;
-  useEffect(() => {
-    const handleScroll = () => {
-      const { scrollTop, scrollHeight, clientHeight } = containerRef.current!;
-      if (scrollTop + clientHeight >= scrollHeight && !isReachingEnd) {
-        setSize((el) => el + 1);
-        console.log("하이");
-      }
-    };
-    const container = containerRef.current!;
-    container.addEventListener("scroll", handleScroll);
-
-    return () => {
-      return container.removeEventListener("scroll", handleScroll);
-    };
-  }, [isReachingEnd, setSize]);
-  const worldcups: MainWorldcup[] = data ? data.map((v) => v.data).flat() : [];
-
-  //예시는 모두다 배열임
-  const isLoadingMore =
-    isLoading || (size > 0 && data && typeof data[size - 1] === "undefined"); //로딩중
-
-  // 더이상 없을때 체크
-  const isRefreshing = isValidating && data && data.length === size; // 요청중
-
-  return (
-    <>
-      <main
-        className="flex h-screen flex-col overflow-y-scroll relative pt-24"
-        ref={containerRef}
-      >
-        <SearchBar setSearch={setSearch} />
-        <div className="mt-12 mx-auto">
-          <SortButtons setSort={setSort} sort={sort} />
-        </div>
-        <article className="w-full h-auto ">
-          {data
-            ? worldcups.map((v) => <Card key={v.id} worldcup={v} />)
-            : new Array(10).fill(1).map((_, i) => {
-                return <CardSkeleton key={i} />;
-              })}
-        </article>
-      </main>
-    </>
-  );
+	return (
+		<main
+			className="flex h-screen flex-col overflow-y-scroll relative pt-24"
+			ref={containerRef}>
+			<SearchBar setSearch={setSearch} />
+			<SortButtons setSort={setSort} sort={sort} />
+			<WorldcupList worldcups={worldcups} isLoading={isLoading} />
+		</main>
+	);
 }
