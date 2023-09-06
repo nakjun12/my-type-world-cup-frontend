4e7acbfb8763b00ebd5338f956b5269b4b78ebf1
nakjun <111031253+nakjun12@users.noreply.github.com>
2023-09-06 16:07:22 +0900

    ga4 위치 수정

--- my-type-world-cup/src/pages/_app.tsx
+++ my-type-world-cup/src/pages/_app.tsx
@@ -14,31 +14,34 @@ const swrConfig = {
 
 function App({ Component, pageProps }: AppProps) {
 	return (
-		<div className="bg-hover flex justify-center items-center mt-[-1px] w-auto overflow-hidden">
-			<Head>
-				<title>이상형 월드컵</title>
-				<link rel="icon" href="/icon/trophy.svg" />
-				<link rel="mask-icon" href="/icon/trophy.svg" color="#000000" />
-				<MetaTags />
-			</Head>
+		<>
 			<GoogleAnalytics />
-			<div className="my-auto h-0 lg:h-auto mt-40 mr-4 hidden lg:block">
-				<p className="mt-4 text-2xl text-left">나의 마음을 확인하세요</p>
-				<h2 className="text-left text-5xl font-bold text-[#117FFA]">
-					이상형 월드컵
-				</h2>
-			</div>
-			<RecoilRoot>
-				<div className="bg-white max-w-[480px] w-full sm:min-w-[480px] mx-auto sm:mx-20 shadow-lg h-5/6">
-					<Header />
-					<div className="-mt-[63px]">
-						<SWRConfig value={swrConfig}>
-							<Component {...pageProps} />
-						</SWRConfig>
-					</div>
+			<div className="bg-hover flex justify-center items-center mt-[-1px] w-auto overflow-hidden">
+				<Head>
+					<title>이상형 월드컵</title>
+					<link rel="icon" href="/icon/trophy.svg" />
+					<link rel="mask-icon" href="/icon/trophy.svg" color="#000000" />
+					<MetaTags />
+				</Head>
+
+				<div className="my-auto h-0 lg:h-auto mt-40 mr-4 hidden lg:block">
+					<p className="mt-4 text-2xl text-left">나의 마음을 확인하세요</p>
+					<h2 className="text-left text-5xl font-bold text-[#117FFA]">
+						이상형 월드컵
+					</h2>
 				</div>
-			</RecoilRoot>
-		</div>
+				<RecoilRoot>
+					<div className="bg-white max-w-[480px] w-full sm:min-w-[480px] mx-auto sm:mx-20 shadow-lg h-5/6">
+						<Header />
+						<div className="-mt-[63px]">
+							<SWRConfig value={swrConfig}>
+								<Component {...pageProps} />
+							</SWRConfig>
+						</div>
+					</div>
+				</RecoilRoot>
+			</div>
+		</>
 	);
 }
 
