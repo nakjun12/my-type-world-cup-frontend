59683afada392e036fc6731c260603db078e8564
nakjun <111031253+nakjun12@users.noreply.github.com>
2023-09-06 17:10:45 +0900

    Revert "ga4 라이브러리 추가"
    This reverts commit a708a55f7674963ae9ed44c6a79ebc45ddfca815.

--- my-type-world-cup/src/pages/_app.tsx
+++ my-type-world-cup/src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import MetaTags from "@/components/MetaTag";
 import Header from "@/components/all/Header";
 import "@/styles/globals.css";
-import GoogleAnalytics from "@bradgarropy/next-google-analytics/dist/types/components/GoogleAnalytics/GoogleAnalytics";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { RecoilRoot } from "recoil";
 import { SWRConfig } from "swr";
+import * as gtag from "../lib/gtag";
 
 const swrConfig = {
 	revalidateOnFocus: false,
@@ -13,15 +15,39 @@ const swrConfig = {
 };
 
 function App({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+	useEffect(() => {
+		const handleRouteChange = (url: URL) => {
+			gtag.pageview(url);
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
 	return (
 		<>
-			<GoogleAnalytics measurementId="G-2MHY2KXJCP" />
 			<div className="bg-hover flex justify-center items-center mt-[-1px] w-auto overflow-hidden">
 				<Head>
 					<title>이상형 월드컵</title>
 					<link rel="icon" href="/icon/trophy.svg" />
 					<link rel="mask-icon" href="/icon/trophy.svg" color="#000000" />
 					<MetaTags />
+					<script
+						async
+						src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+					/>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+           gtag('config', 'G-2MHY2KXJCP');
+            `
+						}}
+					/>
 				</Head>
 
 				<div className="my-auto h-0 lg:h-auto mt-40 mr-4 hidden lg:block">
