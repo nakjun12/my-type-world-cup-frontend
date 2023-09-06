e6bab29d6e4b3b0ef0e95f235fde33acb968ee9b
nakjun <111031253+nakjun12@users.noreply.github.com>
2023-09-06 17:50:27 +0900

    ga4 로직 정리 및 env 연결

--- my-type-world-cup/src/pages/_app.tsx
+++ my-type-world-cup/src/pages/_app.tsx
@@ -1,14 +1,13 @@
 import MetaTags from "@/components/MetaTag";
 import Header from "@/components/all/Header";
+import { GA_TRACKING_ID } from "@/lib/config";
+import { usePageView } from "@/lib/hooks/usePageview";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { useRouter } from "next/router";
 import Script from "next/script";
-import { useEffect } from "react";
 import { RecoilRoot } from "recoil";
 import { SWRConfig } from "swr";
-import * as gtag from "../lib/gtag/gtag"; // 여기서 gtag는 위에서 정의한 Google Analytics 함수를 포함한 파일입니다.
 
 const swrConfig = {
 	revalidateOnFocus: false,
@@ -16,21 +15,7 @@ const swrConfig = {
 };
 
 function App({ Component, pageProps }: AppProps) {
-	const router = useRouter();
-
-	useEffect(() => {
-		const handleRouteChange = (url: URL) => {
-			gtag.pageview(url);
-		};
-
-		// 라우터 이벤트에 이벤트 리스너를 붙입니다.
-		router.events.on("routeChangeComplete", handleRouteChange);
-
-		// 컴포넌트가 언마운트될 때 이벤트 리스너를 제거합니다.
-		return () => {
-			router.events.off("routeChangeComplete", handleRouteChange);
-		};
-	}, [router.events]);
+	usePageView();
 	return (
 		<>
 			<div className="bg-hover flex justify-center items-center mt-[-1px] w-auto overflow-hidden">
@@ -46,14 +31,14 @@ function App({ Component, pageProps }: AppProps) {
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
 
-           gtag('config', 'G-2MHY2KXJCP');
+              gtag('config', '${GA_TRACKING_ID}');
             `
 						}}
 					/>
 				</Head>
 				<Script
 					async
-					src={`https://www.googletagmanager.com/gtag/js?id=G-2MHY2KXJCP`}
+					src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
 				/>
 				<div className="my-auto h-0 lg:h-auto mt-40 mr-4 hidden lg:block">
 					<p className="mt-4 text-2xl text-left">나의 마음을 확인하세요</p>
