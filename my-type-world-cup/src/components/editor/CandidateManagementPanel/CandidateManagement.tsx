b4f35576a17290711ad28e45c82610b1edb4788d
nakjun12 <wns450@gmail.com>
2023-09-26 00:23:54 +0900

    fix : 배포 오류를 인한 수정

--- my-type-world-cup/src/components/editor/CandidateManagementPanel/CandidateManagement.tsx
+++ my-type-world-cup/src/components/editor/CandidateManagementPanel/CandidateManagement.tsx
@@ -1,8 +1,9 @@
 import { useCandidateManagementState } from "@/lib/hooks/useCandidateManagementState";
 import type { Editor_step, Post_res } from "@/type/Types";
 import { Dispatch, SetStateAction, useEffect } from "react";
-import ImageUpload from "./imageupload/ImageUpload";
 import CandidateList from "./candidateList/CandidateList";
+import ImageUpload from "./imageupload/ImageUpload";
+
 type Props = {
   saveWorldcup: Post_res | null;
   setIsNumber: Dispatch<SetStateAction<Editor_step>>;
